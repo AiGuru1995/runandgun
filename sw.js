c3f3e1a18a69aed6a6b50efb69a10413bde0daf9
Abdul Basit <abdulbasitseo1995@gmail.com>
2026-08-29 10:48:29 +0500

    Add original character and background art

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'run-and-gun-v1';
+const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './hero.png', './hero_left.png', './enemy.png', './enemy_left.png', './background.png'];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(cached=>cached || fetch(e.request)));
+});
