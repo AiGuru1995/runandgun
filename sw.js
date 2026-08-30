20c853ffad8cfaa62906f13785015137fc230056
Abdul Basit <abdulbasitseo1995@gmail.com>
2026-08-30 13:28:01 +0500

    Major update: menu, levels, weapons, bosses, sound, and more

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE_NAME = 'run-and-gun-v1';
-const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './hero_green.png', './hero_green_left.png', './hero_blue.png', './hero_blue_left.png', './hero_red.png', './hero_red_left.png', './hero_black.png', './hero_black_left.png', './enemy.png', './enemy_left.png', './background.png'];
+const CACHE_NAME = 'run-and-gun-v3';
+const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png',
+  './hero_green.png', './hero_green_left.png', './hero_blue.png', './hero_blue_left.png',
+  './hero_red.png', './hero_red_left.png', './hero_black.png', './hero_black_left.png',
+  './enemy.png', './enemy_left.png', './enemy_flyer.png', './enemy_flyer_left.png',
+  './enemy_turret.png', './boss.png', './boss_left.png', './background.png'];
 
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES)));
